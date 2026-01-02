647ba1108f0e57394898f3d1e26bf68e29c3cc9a
Markus Westerlind <marwes91@gmail.com>
2026-01-03 00:45:37 +0100

    Fix pins

--- configs/Pico/BoardConfig.h
+++ configs/Pico/BoardConfig.h
@@ -16,15 +16,18 @@
 #define GPIO_PIN_02 GpioAction::BUTTON_PRESS_R2    // UP     | UP     | UP      | UP       | UP     | UP     |
 #define GPIO_PIN_03 GpioAction::BUTTON_PRESS_L1    // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
 #define GPIO_PIN_04 GpioAction::BUTTON_PRESS_B4    // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
-#define GPIO_PIN_05 GpioAction::BUTTON_PRESS_B3    // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
-#define GPIO_PIN_06 GpioAction::BUTTON_PRESS_B1    // B1     | A      | B       | Cross    | 2      | K1     |
-#define GPIO_PIN_07 GpioAction::BUTTON_PRESS_B2    // B2     | B      | A       | Circle   | 3      | K2     |
+#define GPIO_PIN_05 GpioAction::BUTTON_PRESS_B1    // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
+
+#define GPIO_PIN_06 GpioAction::BUTTON_PRESS_B3    // B1     | A      | B       | Cross    | 2      | K1     |
+#define GPIO_PIN_07 GpioAction::BUTTON_PRESS_B1    // B2     | B      | A       | Circle   | 3      | K2     |
 #define GPIO_PIN_08 GpioAction::BUTTON_PRESS_S1    // R2     | RT     | ZR      | R2       | 8      | K3     |
 #define GPIO_PIN_09 GpioAction::BUTTON_PRESS_S2    // L2     | LT     | ZL      | L2       | 7      | K4     |
+
 #define GPIO_PIN_10 GpioAction::BUTTON_PRESS_UP    // B3     | X      | Y       | Square   | 1      | P1     |
 #define GPIO_PIN_11 GpioAction::BUTTON_PRESS_RIGHT // B4     | Y      | X       | Triangle | 4      | P2     |
 #define GPIO_PIN_12 GpioAction::BUTTON_PRESS_DOWN  // R1     | RB     | R       | R1       | 6      | P3     |
 #define GPIO_PIN_13 GpioAction::BUTTON_PRESS_LEFT  // L1     | LB     | L       | L1       | 5      | P4     |
+
 #define GPIO_PIN_16 GpioAction::BUTTON_PRESS_S1    // S1     | Back   | Minus   | Select   | 9      | Coin   |
 #define GPIO_PIN_17 GpioAction::BUTTON_PRESS_S2    // S2     | Start  | Plus    | Start    | 10     | Start  |
 #define GPIO_PIN_18 GpioAction::BUTTON_PRESS_L3    // L3     | LS     | LS      | L3       | 11     | LS     |
